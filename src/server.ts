2003e6c9847c58472315cd6ae80ccaffec56090f
jnarowski <jnarowski@gmail.com>
2019-08-06 13:20:05 -0600

    added dev dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
 import log from "./log";
+const port = process.env.PORT || 5000;
 
-const app = express();
+const app: express.Application = express();
 
 app.get("/", (req, res) => {
-    res.json(JSON.stringify({ok: 1})).end();
+  res.json(JSON.stringify({ ok: 1, better: 2 })).end();
 });
 
 app.listen(process.env.PORT || 5000, () => {
-    log.info("app running");
+  log.info(`server started at http://localhost:${port}`);
 });
